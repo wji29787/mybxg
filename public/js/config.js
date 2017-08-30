93d907322e67b0049a77d7af7ddcf12a8bbea67a
wji29787 <wji29787@163.com>
2017-08-31 00:38:42 +0800

    教师查看预览功能

--- public/js/config.js
+++ public/js/config.js
@@ -4,8 +4,14 @@ require.config({
        jquery:'jquery/jquery.min',
        cookie:'jquery-cookie/jquery.cookie',
        template:'artTemplate/art-template',
+       bootstrap:'bootstrap/js/bootstrap.min',
        login:'../js/login',
        common:'../js/common',
        teachterlist:'../js/teachter-list'
+    },
+    shim :{
+       bootstrap:{
+           deps:['jquery']
+       }
     }
 });
--- public/js/teachter-list.js
+++ public/js/teachter-list.js
@@ -1,4 +1,4 @@
-define(['jquery','template'],function ($,template) {
+define(['jquery','template','bootstrap'],function ($,template) {
     //请求后台借口获取列表数据
     $.ajax({
         type:'get',
@@ -8,6 +8,28 @@ define(['jquery','template'],function ($,template) {
         //    解析数据并渲染页面
         var html=template('teacherTpl',{list:data.result});
         $('#teacherInfo').html(html);
+        //绑定预览的点击事件
+        $('.preview').click(function () {
+            //通过后台借口获取数据
+            var tcId=$(this).closest('tr').attr('data-tcid');//找到离他最近的父元素
+            // console.log(tcId);
+            $.ajax({
+               type:'get',
+                url:'/api/teacher/view',
+                data:{
+                   tc_id:tcId
+                },
+                dataType:'json',
+                success:function (data) {
+                   console.log(data);
+                   var html=template('modalTpl',data.result);
+                    // console.log(html);
+                    $('#modalInfo').html(html);
+                //   显示弹窗
+                    $('#teacherModal').modal();
+                }
+            })
+        });
         }
     });
 });
