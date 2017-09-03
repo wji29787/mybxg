fd4925a0b9881e853fb45e4cb8aba535152a9fc6
wji29787 <wji29787@163.com>
2017-09-03 20:46:34 +0800

    完成头像上传功能

--- public/js/settings.js
+++ public/js/settings.js
@@ -1,4 +1,4 @@
-define(['jquery','template','util'],function ($,template,util) {
+define(['jquery','template','util','datepicker','language','uploadify'],function ($,template,util) {
      //设置导航菜单选中
      util.setMenu('/main/index');
 //     调用后台借口获取所有的信息
@@ -10,6 +10,22 @@ define(['jquery','template','util'],function ($,template,util) {
             // console.log(data);
             var html=template('settingsTpl',data.result);
             $('#settingsInfo').html(html);
+        //    处理头像上传
+            $('#upfile').uploadify({
+                width:120,
+                height:120,
+                buttonText:'',
+                itemTemplate:'<span></span>',
+                fileObjName:'tc_avatar',
+                swf:'/public/assets/uploadify/uploadify.swf',
+                uploader:'/api/uploader/avatar',
+                onUploadSuccess:function (f,data) {
+                    var data=JSON.parse(data);
+                    // console.log(data);
+                   $('.preview img').attr('src',data.result.path);
+                    console.log(data.result.path);
+                }
+            })
         }
     });
 });
